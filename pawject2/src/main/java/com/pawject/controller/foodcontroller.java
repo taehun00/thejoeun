a07d55886d94e8c4c7cc9caae190bded3326c464
tkasid00 <tkasid00@gmail.com>
2025-11-25 18:06:38 +0900

    컨트롤러추가

---
+++ pawject2/src/main/java/com/pawject/controller/foodcontroller.java
@@ -0,0 +1,11 @@
+package com.pawject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class foodcontroller {
+	public void test() {
+		System.out.println("test");
+	}
+
+}
