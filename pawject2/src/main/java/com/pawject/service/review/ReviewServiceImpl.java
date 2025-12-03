9805d7e73f814b7243ff67e1d1e2c35433862d35
tkasid00 <tkasid00@gmail.com>
2025-12-03 14:47:04 +0900

    1203중간저장

--- pawject2/src/main/java/com/pawject/service/review/ReviewServiceImpl.java
+++ pawject2/src/main/java/com/pawject/service/review/ReviewServiceImpl.java
@@ -52,34 +52,65 @@ public class ReviewServiceImpl implements ReviewService {
 		return idao.reviewimgSelect(reviewid);
 	}
 
-	public int reviewimginsert(int reviewid, List<MultipartFile> files) {
-		
-		int successcnt=0;
-		
-	    for (MultipartFile file : files) {
-	        if (file.isEmpty()) { continue; }  //!아님 이미지 없으면 스킵하고 업로드
-	        String uuid = UUID.randomUUID().toString();
-	        String originName = file.getOriginalFilename();
-	        String fileName = uuid + "_" + originName; //이거 안하면 db에서 이미지 이름 중복 시 충돌남
-	        String uploadPath = "C:/file/";
+	@Override
+	public ReviewImgDto reviewimginsert(int reviewid,  MultipartFile file) {
+		//  ㄴint 아님 주의!
 
-	        File img = new File(uploadPath + fileName);
+		if (file == null || file.isEmpty()) {
+		    return null;
+		}
 
-	        try {
-	            file.transferTo(img);
-	            successcnt++;  //빼먹으면 카운트 0이됨 영원히....
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
+		String uuid = UUID.randomUUID().toString();
+        String originName = file.getOriginalFilename();
+        String fileName = uuid + "_" + originName;
+        String uploadPath = "C:/file/";
 
-	        ReviewImgDto dto = new ReviewImgDto();
-	        dto.setReviewid(reviewid);
-	        dto.setReviewimgname(fileName);
+        File img = new File(uploadPath + fileName);
 
-	        idao.reviewimginsert(dto);
-	    }//for
-	    return successcnt;
+	    try {
+	        file.transferTo(img);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    ReviewImgDto dto = new ReviewImgDto();
+	    dto.setReviewid(reviewid);
+	    dto.setReviewimgname(fileName);
+
+	    idao.reviewimginsert(dto);
+
+	    return dto;
 	}
+	
+	
+	//mvc 버전 - 일괄 업로드
+//	public int reviewimginsert(int reviewid, List<MultipartFile> files) {
+//		int successcnt=0;
+//		
+//	    for (MultipartFile file : files) {
+//	        if (file.isEmpty()) { continue; }  //!아님 이미지 없으면 스킵하고 업로드
+//	        String uuid = UUID.randomUUID().toString();
+//	        String originName = file.getOriginalFilename();
+//	        String fileName = uuid + "_" + originName; //이거 안하면 db에서 이미지 이름 중복 시 충돌남
+//	        String uploadPath = "C:/file/";
+//
+//	        File img = new File(uploadPath + fileName);
+//
+//	        try {
+//	            file.transferTo(img);
+//	            successcnt++;  //빼먹으면 카운트 0이됨 영원히....
+//	        } catch (Exception e) {
+//	            e.printStackTrace();
+//	        }
+//
+//	        ReviewImgDto dto = new ReviewImgDto();
+//	        dto.setReviewid(reviewid);
+//	        dto.setReviewimgname(fileName);
+//
+//	        idao.reviewimginsert(dto);
+//	    }//for
+//	    return successcnt;
+//	}
 	
 	@Override
 	public int reviewimgupdate(int reviewid, List<MultipartFile> files) {
@@ -111,8 +142,28 @@ public class ReviewServiceImpl implements ReviewService {
 	    return successcnt;
 	}
 
+	//파일 삭제 전용 메서드
+	private void filedelete(String fileName) {
+		String uploadPath = "C:/file/";
+		File img = new File(uploadPath + fileName);
+		
+		if(img.exists()) {
+			img.delete();
+		}
+	}
+	
+	
+	
 	@Override
 	public int reviewimgdeleteAll(int reviewid) {
+		
+		List<ReviewImgDto> img = idao.reviewimgSelect(reviewid);
+		
+	    for (ReviewImgDto dto : img) {
+	    		String fileName=dto.getReviewimgname();
+	    		filedelete(dto.getReviewimgname());
+	    }
+		
 		return idao.reviewimgdeleteAll(reviewid);
 	}
 
