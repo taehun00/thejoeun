56e641345e6f9dc361890cf9c4347479131c8975
Tae Hun <iiss07007@gmail.com>
2025-12-23 18:16:56 +0900

    .

---
+++ pawject3/src/main/java/com/pawject/Pawject3Application.java
@@ -0,0 +1,13 @@
+package com.pawject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pawject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pawject3Application.class, args);
+	}
+
+}
---
+++ pawject3/src/main/java/com/pawject/dto/paging/PagingDto10.java
@@ -0,0 +1,37 @@
+package com.pawject.dto.paging;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PagingDto10 {
+	private int listtotal;
+	private int onepagelist;
+	private int pagetotal;
+	private int bottomlist;
+	private int pstartno;
+	
+	private int current;
+	private int start;
+	private int end;
+	
+	public PagingDto10(int listtotal, int pstartno) {
+		super();
+		this.listtotal = listtotal;
+			if(listtotal<=0) {listtotal=1;}
+		this.onepagelist = 10;
+		this.pagetotal = (int)Math.ceil((double)listtotal/onepagelist);
+		this.bottomlist = 10;
+		this.pstartno = (pstartno-1)*onepagelist+1;
+		
+		this.current = pstartno;
+		this.start = ((current-1)/bottomlist)*bottomlist+1;
+		this.end = start+bottomlist-1;
+			if(end>pagetotal) {end=pagetotal;}
+	}
+	
+	
+	
+
+}
