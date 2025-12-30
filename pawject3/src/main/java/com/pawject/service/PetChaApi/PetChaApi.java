204f15cbcf1418119ce85d9d28504bc8bb7db187
SangwookChoi <gforce74@naver.com>
2025-12-30 18:18:36 +0900

    ..

---
+++ pawject3/src/main/java/com/pawject/service/PetChaApi/PetChaApi.java
@@ -0,0 +1,12 @@
+package com.pawject.service.PetChaApi;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PetChaApi {
+	
+	@Value("${openai.api.key.swc}")
+	private String PetChaApiKey;
+
+}
