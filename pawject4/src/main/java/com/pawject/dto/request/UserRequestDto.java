5bc0cc8c1610137c42c3a34113b9e02bb0b50aeb
Tae Hun <iiss07007@gmail.com>
2026-01-22 18:18:03 +0900

    .

---
+++ pawject4/src/main/java/com/pawject/dto/request/UserRequestDto.java
@@ -0,0 +1,24 @@
+package com.pawject.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDto {
+
+    private String email;
+    private String password;
+    private String nickname;
+
+    // 소셜 로그인용
+    private String provider;    // local, google, kakao ...
+    private String providerId;  // 소셜 제공자 ID
+
+}
