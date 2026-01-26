dda801a40491fe723ce082fe479a2cde8943a1eb
tkasid00 <tkasid00@gmail.com>
2026-01-26 20:28:26 +0900

    복원

--- pawject4_front/reducers/index.js
+++ pawject4_front/reducers/index.js
@@ -1,9 +1,11 @@
 // reducers/index.js
 import { combineReducers } from "@reduxjs/toolkit";
 import foodReducer from "./food/foodReducer";
+import foodReducer from "./review/reviewReducer";
 
 const rootReducer = combineReducers({
   food: foodReducer,
+  review: reviewReducer,
 });
 
 export default rootReducer;
