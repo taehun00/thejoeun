f146dbabb09b2ff59e4934c8ac32a67a9eb6706d
Seunghyun Han <h48097435@gmail.com>
2026-01-28 09:41:06 +0900

    임시커밋_승현

--- pawject4/src/main/java/com/pawject/domain/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.pawject.domain;
-
-public enum RoleType {
-    ROLE_MEMBER,
-    ROLE_ADMIN
-}
--- pawject4/src/main/java/com/pawject/repository/PetTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pawject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pawject.domain.PetType;
-
-public interface PetTypeRepository extends JpaRepository<PetType, Long> {
-}
