1cf3e81c706f9494606a338df1ea750e14a36881
tkasid00 <tkasid00@gmail.com>
2026-02-03 11:51:31 +0900

    광고병합

--- pawject4/pawject4_front/components/AppLayout.js
+++ pawject4/pawject4_front/components/AppLayout.js
@@ -53,13 +53,15 @@ export default function AppLayout({ children }) {
       { key: "/disease", label: <Link href="/petdisease">질환정보</Link> },
       { key: "/tester", label: <Link href="/tester">체험단</Link> },
       { key: "/faq", label: <Link href="/faq">고객센터</Link> },
+      //{ key: "/ad", label: <Link href="/ad">광고</Link> },  // 광고 기능 작동여부 확인용.
     ];
 
     // 관리자 전용 메뉴
     if (canAdmin) {
       items.push(
         { key: "/foodboard", label: <Link href="/foodboard">사료관리</Link> },
-        { key: "/faq/admin", label: <Link href="/faq/admin">FAQ관리</Link> }
+        { key: "/faq/admin", label: <Link href="/faq/admin">FAQ관리</Link> },
+        { key: "/ad", label: <Link href="/ad">광고관리</Link> }  // 관리자가 광고 관리
       );
     }
 
